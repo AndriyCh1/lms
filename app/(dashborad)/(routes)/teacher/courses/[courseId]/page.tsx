98bc4c10e0716ca09f963ed81b9fd2bd6fc8a2b2
AndriyCh1 <chekanovskiyacc@gmail.com>
2024-09-21 10:38:06 -0600

    feat: publish and unpublish course

--- app/(dashborad)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashborad)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -15,6 +15,8 @@ import { CategoryForm } from "./_components/category-form";
 import { PriceForm } from "./_components/price-form";
 import { AttachmentForm } from "./_components/attachment-form";
 import { ChaptersForm } from "./_components/chapters-form";
+import { Banner } from "@/components/banner";
+import { CourseActions } from "./_components/course-actions";
 
 export default async function CourseIdPage({
   params,
@@ -67,73 +69,83 @@ export default async function CourseIdPage({
 
   const totalFields = requiredFields.length;
   const completedFields = requiredFields.filter(Boolean).length;
-
   const completionText = `(${completedFields}/${totalFields})`;
+  const isCompleted = requiredFields.every(Boolean);
 
   return (
-    <div className="p-6">
-      <div className="flex items-center justify-between">
-        <div className="flex flex-col gap-y-2">
-          <h1 className="text-2xl font-medium">Course setup</h1>
-          <span className="text-sm text-slate-700">
-            Complete all fields {completionText}
-          </span>
-        </div>
-      </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
-        <div>
-          <div className="flex items-center gap-x-2">
-            <IconBadge icon={LayoutDashboardIcon} />
-            <h2 className="text-xl">Customize your course</h2>
+    <>
+      {!course.isPublished && (
+        <Banner label="This course is unpublished. It will not be visible to the students." />
+      )}
+      <div className="p-6">
+        <div className="flex items-center justify-between">
+          <div className="flex flex-col gap-y-2">
+            <h1 className="text-2xl font-medium">Course setup</h1>
+            <span className="text-sm text-slate-700">
+              Complete all fields {completionText}
+            </span>
           </div>
-          <TitleForm
-            initialData={{ title: course.title }}
+          <CourseActions
+            disabled={!isCompleted}
             courseId={course.id}
-          />
-          <DescriptionForm
-            initialData={{ description: course.description || undefined }}
-            courseId={course.id}
-          />
-          <ImageForm
-            initialData={{ imageUrl: course.imageUrl || undefined }}
-            courseId={course.id}
-          />
-          <CategoryForm
-            initialData={{ categoryId: course.categoryId || undefined }}
-            courseId={course.id}
-            options={categories.map((category) => ({
-              label: category.name,
-              value: String(category.id),
-            }))}
+            isPublished={course.isPublished}
           />
         </div>
-        <div className="space-y-6">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
           <div>
             <div className="flex items-center gap-x-2">
-              <IconBadge icon={ListChecks} />
-              <h2 className="text-xl">Course chapters</h2>
+              <IconBadge icon={LayoutDashboardIcon} />
+              <h2 className="text-xl">Customize your course</h2>
             </div>
-            <ChaptersForm initialData={course} courseId={course.id} />
-          </div>
-          <div>
-            <div className="flex items-center gap-x-2">
-              <IconBadge icon={CircleDollarSign} />
-              <h2 className="text-xl">Sell your course</h2>
-            </div>
-            <PriceForm
+            <TitleForm
+              initialData={{ title: course.title }}
               courseId={course.id}
-              initialData={{ price: course.price || undefined }}
+            />
+            <DescriptionForm
+              initialData={{ description: course.description || undefined }}
+              courseId={course.id}
+            />
+            <ImageForm
+              initialData={{ imageUrl: course.imageUrl || undefined }}
+              courseId={course.id}
+            />
+            <CategoryForm
+              initialData={{ categoryId: course.categoryId || undefined }}
+              courseId={course.id}
+              options={categories.map((category) => ({
+                label: category.name,
+                value: String(category.id),
+              }))}
             />
           </div>
-          <div>
-            <div className="flex items-center gap-x-2">
-              <IconBadge icon={File} />
-              <h2 className="text-xl">Resources & attachments</h2>
+          <div className="space-y-6">
+            <div>
+              <div className="flex items-center gap-x-2">
+                <IconBadge icon={ListChecks} />
+                <h2 className="text-xl">Course chapters</h2>
+              </div>
+              <ChaptersForm initialData={course} courseId={course.id} />
             </div>
-            <AttachmentForm initialData={course} courseId={course.id} />
+            <div>
+              <div className="flex items-center gap-x-2">
+                <IconBadge icon={CircleDollarSign} />
+                <h2 className="text-xl">Sell your course</h2>
+              </div>
+              <PriceForm
+                courseId={course.id}
+                initialData={{ price: course.price || undefined }}
+              />
+            </div>
+            <div>
+              <div className="flex items-center gap-x-2">
+                <IconBadge icon={File} />
+                <h2 className="text-xl">Resources & attachments</h2>
+              </div>
+              <AttachmentForm initialData={course} courseId={course.id} />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
--- app/api/courses/[courseId]/route.ts
+++ app/api/courses/[courseId]/route.ts
@@ -1,6 +1,66 @@
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
+import Mux from "@mux/mux-node";
+
+const { video } = new Mux({
+  tokenId: process.env.MUX_TOKEN_ID!,
+  tokenSecret: process.env.MUX_TOKEN_SECRET!,
+});
+
+export async function DELETE(
+  req: Request,
+  { params }: { params: { courseId: string } }
+) {
+  try {
+    const { userId } = auth();
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const course = await db.course.findUnique({
+      where: {
+        id: +params.courseId,
+        userId: userId,
+      },
+      include: {
+        chapters: {
+          include: {
+            muxData: true,
+          },
+        },
+      },
+    });
+
+    if (!course) {
+      return new NextResponse("Not found", { status: 404 });
+    }
+
+    const deleteChapterVideoPromises = await Promise.allSettled(
+      course.chapters.map((chapter) => {
+        if (chapter.muxData?.assetId) {
+          return video.assets.delete(chapter.muxData.assetId);
+        }
+      })
+    );
+
+    if (deleteChapterVideoPromises.some((p) => p.status === "rejected")) {
+      return new NextResponse("Internal Error", { status: 500 });
+    }
+
+    const deletedCourse = await db.course.delete({
+      where: {
+        id: +params.courseId,
+      },
+    });
+
+    return NextResponse.json(deletedCourse);
+  } catch (error) {
+    console.log("[COURSE_ID_DELETE]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
 
 export const PATCH = async (
   req: Request,
